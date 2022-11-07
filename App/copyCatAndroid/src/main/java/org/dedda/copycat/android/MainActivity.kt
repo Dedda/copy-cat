b1881583e572144a9e029428ebe6471ea46fa510
Stephan Göppentin <dedda102@gmail.com>
2022-11-07 20:28:38 +0100

    a bit more bottom navigation sugar

--- App/copyCatAndroid/src/main/java/org/dedda/copycat/android/MainActivity.kt
+++ App/copyCatAndroid/src/main/java/org/dedda/copycat/android/MainActivity.kt
@@ -7,14 +7,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import org.dedda.copycat.android.sampledata.SampleRepository
@@ -59,7 +67,9 @@ fun MainContents(
                 composable("servers") {
                     ServerListContents(
                         repo,
-                        onNavigateToAddServer = { navControler.navigate("addServer") })
+                        onNavigateToAddServer = { navControler.navigate("addServer") },
+                        onNavigateToEditServer = { serverId -> navControler.navigate("editServer/$serverId") }
+                    )
                 }
                 composable("addServer") {
                     AddServerContents(
@@ -73,33 +83,38 @@ fun MainContents(
                 ) { backStackEntry ->
                     EditServer(repo, serverId = backStackEntry.arguments!!.getLong("serverId"))
                 }
+                composable("settings") {
+                    SettingsContents()
+                }
             }
         },
         bottomBar = {
             BottomNavigation {
-                BottomNavigationItem(selected = true,
-                    onClick = {
-                        navControler.backQueue.clear()
-                        navControler.navigate("home")
-                    },
-                    icon = {
-
-                    },
-                    label = {
-                        Text(text = "Home")
-                    })
-                BottomNavigationItem(selected = false,
-                    onClick = {
-                        navControler.backQueue.clear()
-                        navControler.navigate("servers")
-                    },
-                    icon = {
-
-                    },
-                    label = {
-                        Text(text = "Servers")
-                    })
+                val navBackEntry by navControler.currentBackStackEntryAsState()
+                val currentRoute = navBackEntry?.destination?.route
+                BottomNavItem.values().forEach { navItem ->
+                    BottomNavigationItem(
+                        selected = currentRoute == navItem.route,
+                        onClick = {
+                            navControler.backQueue.clear()
+                            navControler.navigate(navItem.route)
+                        },
+                        icon = { Icon(navItem.icon, navItem.contentDescription) },
+                        label = { Text(text = navItem.label) },
+                    )
+                }
             }
         }
     )
+}
+
+private enum class BottomNavItem(
+    val route: String,
+    val icon: ImageVector,
+    val contentDescription: String,
+    val label: String
+) {
+    HomeNavItem("home", Icons.Filled.Home, "Home icon", "Home"),
+    ServerListNavItem("servers", Icons.Filled.List, "Server list icon", "Servers"),
+    SettingsNavItem("settings", Icons.Filled.Settings, "Settings icon", "Settings")
 }
---
+++ App/copyCatAndroid/src/main/java/org/dedda/copycat/android/Settings.kt
@@ -0,0 +1,8 @@
+package org.dedda.copycat.android
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsContents() {
+
+}
